669a238e0116f2b428efe587ee772a26658b0b28
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2017-12-11 18:18:39 -0300

    fix: Fix use of y_test and y_pred to evaluate accuracy

--- gen_dataset.py
+++ gen_dataset.py
@@ -37,16 +37,20 @@ def gen_fake(n_total=4039, n_genuines=1399):
 def friend_pattern(px_friend_list, genuine_ix):
     new_friend_list = []
     n_genuines = 0
-    n_fakes = 0
+    n_genuines = []
+    # n_fakes = 0
+    n_fakes = []
     genuine_fp = '(a|b)*a#'  # Facebook FP
     fake_fp = '(a*|b)(b|ab*a)#'
     for i, inst in enumerate(px_friend_list):
         if int(inst) in genuine_ix:
             fp_to_use = genuine_fp
-            n_genuines += 1
+            # n_genuines += 1
+            n_genuines.append('genuine')
         else:
             fp_to_use = fake_fp
-            n_fakes += 1
+            # n_fakes += 1
+            n_fakes.append('fake')
         while True:
             fp = rstr.xeger(fp_to_use)
             if fp not in new_friend_list:
@@ -84,21 +88,27 @@ if __name__ == '__main__':
     ds = gen_dataset(fname)
     genuine_ix, fake_ix = gen_fake()
     test_genuine_count = 0
+    y_test = []
     test_fake_count = 0
     genuine_count = 0
+    y_pred = []
     fake_count = 0
     for i, (px, fl) in enumerate(ds.items()):
         # print('Working on profile px = {}'.format(px))
         fl_intances, n_g, n_f = friend_pattern(ds[px], genuine_ix)
-        test_genuine_count += n_g
-        test_fake_count += n_f
-        for ix in fl_intances:
+        test_genuine_count += len(n_g)
+        test_fake_count += len(n_f)
+        y_test.extend(n_g)
+        y_test.extend(n_f)
+        for px_i, ix in enumerate(fl_intances):
             if fb_ipr.check_profile(ix):
                 genuine_count += 1
+                y_pred.append('genuine')
             else:
                 fake_count += 1
-        # if i == 50:
-        #     break
+                y_pred.append('fake')
+        if i == 50:
+            break
     print('Number of test genuine profiles: {}'.format(test_genuine_count))
     print('Number of predicted genuine profiles: {}'.format(genuine_count))
     print('Number of test fake profiles: {}'.format(test_fake_count))
